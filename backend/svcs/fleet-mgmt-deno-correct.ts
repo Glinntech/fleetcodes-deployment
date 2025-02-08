983d5598191889a58f5b5eaa21c93093b24ef2f1
aman gulati <amangulati075@gmail.com>
2025-02-09 00:35:52 +0530

    [Github-3] https://github.com/Glinntech/fleet-management-backend/issues/3 update image and docker file issue.

--- backend/svcs/fleet-mgmt-deno-correct.ts
+++ backend/svcs/fleet-mgmt-deno-correct.ts
@@ -9,7 +9,7 @@ const containerName = serviceName;
 const containerPort = 8080;
 
 
-const releaseTag = "release-0.0.4";
+const releaseTag = "release-0.0.5";
 
 const executionRole = new aws.iam.Role("ecs-execution-role", {
     assumeRolePolicy: aws.iam.assumeRolePolicyForPrincipal({Service: "ecs-tasks.amazonaws.com"})
